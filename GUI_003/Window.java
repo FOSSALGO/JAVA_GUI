132dc55b271f6c31f16c5759d6d18f82d24737ff
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2022-12-12 10:50:31 +0800

    Update Window.java

--- GUI_003/Window.java
+++ GUI_003/Window.java
@@ -1,6 +1,7 @@
 package fosalgo;
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -13,65 +14,69 @@ import javax.swing.JTextField;
 
 public class Window {
 
-    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final int n = 23;
-    private static final int size = ALPHABET.length();//26
+    public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final int n = 23;
+    public static final int size = ALPHABET.length();//26
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Aplikasi GUI");
-        frame.setSize(600, 100);//atur lebar dan tinggi window
+        JFrame frame = new JFrame("Crypto Caesar Cipher");
+        frame.setSize(600, 300);//atur lebar dan tinggi window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(Color.decode("#d35400"));
+        ImageIcon imageIcon = new ImageIcon("logo.png");
+        frame.setIconImage(imageIcon.getImage());
 
-        frame.getContentPane().setLayout(new FlowLayout());
+        Container contentPane = frame.getContentPane();
+        contentPane.setBackground(Color.decode("#d35400"));
+        contentPane.setLayout(new FlowLayout());
 
         JLabel jLabel1 = new JLabel("Plaintext");
-        frame.getContentPane().add(jLabel1);
+        contentPane.add(jLabel1);
 
         JTextField jTextField1 = new JTextField();
         jTextField1.setPreferredSize(new Dimension(100, 28));
-        frame.getContentPane().add(jTextField1);
+        contentPane.add(jTextField1);
 
         JButton jButton1 = new JButton("Encrypt");
-        frame.getContentPane().add(jButton1);
+        contentPane.add(jButton1);
 
         JButton jButton2 = new JButton("Decrypt");
-        frame.getContentPane().add(jButton2);
+        contentPane.add(jButton2);
 
         JLabel jLabel2 = new JLabel("Ciphertext");
-        frame.getContentPane().add(jLabel2);
+        contentPane.add(jLabel2);
 
         JTextField jTextField2 = new JTextField();
         jTextField2.setPreferredSize(new Dimension(100, 28));
-        frame.getContentPane().add(jTextField2);
+        contentPane.add(jTextField2);
 
-        //AKSI TOMBOL
+        //AKSI
         jButton1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                System.out.println("ENCRYPT");
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 String plaintext = jTextField1.getText();
                 plaintext = plaintext.toUpperCase();
                 String ciphertext = "";
                 for (int i = 0; i < plaintext.length(); i++) {
                     char cin = plaintext.charAt(i);
-                    int x = ALPHABET.indexOf(cin);
+                    int x = ALPHABET.indexOf(cin);//mencari posisi huruf di alphabet
                     int ex = (x + n) % size;
                     char cout = ALPHABET.charAt(ex);
-                    ciphertext = ciphertext + cout;
+                    ciphertext = ciphertext + cout;//konkatenasi
                 }
                 jTextField2.setText(ciphertext);
             }
+
         });
 
         jButton2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                System.out.println("ENCRYPT");
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 String ciphertext = jTextField2.getText();
                 ciphertext = ciphertext.toUpperCase();
                 String plaintext = "";
                 for (int i = 0; i < ciphertext.length(); i++) {
                     char cin = ciphertext.charAt(i);
-                    int x = ALPHABET.indexOf(cin);
+                    int x = ALPHABET.indexOf(cin);//mencari posisi huruf di alphabet
                     int dx = (x - n) % size;
                     if (dx < 0) {
                         dx = size + dx;
@@ -80,11 +85,11 @@ public class Window {
                     plaintext = plaintext + cout;
                 }
                 jTextField1.setText(plaintext);
+
             }
+
         });
 
-        ImageIcon imageIcon = new ImageIcon("logo.png");
-        frame.setIconImage(imageIcon.getImage());
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
