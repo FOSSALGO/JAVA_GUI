d5bee2bd7e8769cac957b57986fa65333ea93510
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2022-12-17 17:39:47 +0800

    Create FTextField.java

---
+++ GUI_011/FTextField.java
@@ -0,0 +1,76 @@
+/*
+ *  Jangan lupa SUBSCRIBE https://www.youtube.com/@FOSALGO/videos
+ *  Tetap Semangat...!!! Semua orang Bisa Belajar Coding
+ *  Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package fosalgo;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author FOSALGO
+ */
+public class FTextField extends JTextField{
+    private Color fillColor;
+    private Color lineColor;
+    private int strokeWidth;
+       
+    public FTextField() {
+        fillColor = new Color(236, 240, 241);
+        lineColor = new Color(52, 152, 219);
+        strokeWidth = 2;
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
+    }
+
+    public Color getFillColor() {
+        return fillColor;
+    }
+
+    public void setFillColor(Color fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    public Color getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+    
+    
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (!isOpaque()) {            
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            int s = strokeWidth;
+            int w = getWidth() - (2 * s);
+            int h = getHeight() - (2 * s);
+            g2d.setColor(fillColor);
+            g2d.fillRoundRect(s, s, w, h, h, h);
+            g2d.setStroke(new BasicStroke(s));
+            g2d.setColor(lineColor);
+            g2d.drawRoundRect(s, s, w, h, h, h);
+        }
+        super.paintComponent(g);        
+    }
+    
+}
